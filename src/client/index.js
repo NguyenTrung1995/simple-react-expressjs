a1741337e86453c41c9fc910a69c0e2d44f8de23
TrungNQ1 <trung.nguyen@gmail.com>
2018-11-25 10:46:45 +0700

    dsfdf

--- src/client/index.js
+++ src/client/index.js
@@ -4,19 +4,19 @@ import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 
-import Signin from "./js/containers/signin.js";
-import Signup from "./js/containers/signup.js";
-import Navigation from "./js/containers/navigation.js";
-import Error from "./js/containers/error.js";
-import Home from "./js/containers/home.js";
-import Root from "./js/containers/root.js";
-import StyleGuide from "./js/containers/styleguide.tsx";
+import Signin from "./js/containers/signin";
+import Signup from "./js/containers/signup";
+import Navigation from "./js/containers/navigation";
+import Error from "./js/containers/error";
+import Home from "./js/containers/home";
+import Root from "./js/containers/root";
+import StyleGuide from "./js/containers/styleguide";
 
 import reducers from './js/reducer';
-import Account from "./js/containers/account.js";
-import Cart from "./js/containers/cart/cart.js";
-import Header from "./js/components/Header.tsx";
-import DetailProduct from "./js/containers/product_detail.tsx";
+import Account from "./js/containers/account";
+import Cart from "./js/containers/cart/cart";
+import Header from "./js/components/Header";
+import DetailProduct from "./js/containers/product_detail";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
