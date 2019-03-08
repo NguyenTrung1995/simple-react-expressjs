38b90d68d6607cb379fd2da61314c9cbf61cf427
TrungNQ1 <trung.nguyen@gmail.com>
2019-03-08 16:28:27 +0700

    refactor

--- src/server/upload.js
+++ src/server/upload.js
@@ -3,18 +3,23 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     if (Object.keys(req.files).length == 0) {
-        return res.status(400).send('No files were uploaded.');
+        return res.status(400).send('No files were uploaded!');
     }
     const uploadFile = req.files.file;
-    const fileName = req.files.file.name;
-    uploadFile.mv(`./public/files/${fileName}`, function(err) {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json({
-            file: `public/files/${req.files.file.name}`,
-        })
-    });
+    if (/^image\//.test(uploadFile.mimetype)) {
+        const fileName = req.files.file.name;
+        uploadFile.mv(`./public/files/${fileName}`, function(err) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            return res.json({
+                file: `public/files/${req.files.file.name}`,
+            });
+        });
+    }
+    else {
+        return res.send('Type of image incorrectly!');
+    }
 })
 
 module.exports = router;
